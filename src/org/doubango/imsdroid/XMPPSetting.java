c72a9ddf5c70c0c0d6c45d88e1316835fbe58242
terry <agoodboy30225@gmail.com>
2015-05-22 10:13:28 +0800

    [DEMO] Correct demo behavior

--- src/org/doubango/imsdroid/XMPPSetting.java
+++ src/org/doubango/imsdroid/XMPPSetting.java
@@ -325,18 +325,17 @@ public class XMPPSetting {
 	
 	private void selectDemoMode(String temp){
 		int demo_x, demo_y;
-		
-		if(temp == "a"){
+		if(temp.equals("a")){
+			demo_x = 400;
+			demo_y = 250;
+			workflowforDemo(demo_x, demo_y);
+		}
+		else if(temp.equals("b")){
 			demo_x=0;
 			demo_y=0;
 			workflowforDemo(demo_x, demo_y);
 		}
-		else if(temp == "b"){
-			demo_x=0;
-			demo_y=0;
-			workflowforDemo(demo_x, demo_y);
-		}
-		else if(temp == "c"){
+		else if(temp.equals("c")){
 			demo_x=0;
 			demo_y=0;
 			workflowforDemo(demo_x, demo_y);
@@ -345,10 +344,10 @@ public class XMPPSetting {
 	}
 	
 	private void workflowforDemo(int demo_x, int demo_y){
-		obj.transform2ScreenGird(demo_x, demo_y);
-		int[][] tempTarget = {{obj.getX_grid(), obj.getY_grid()}};
-		RobotOperationMode.targetQueue.offer(tempTarget);
-		
+	    obj.transform2ScreenGird(demo_x, demo_y);
+	    MapList.target[0] = obj.getX_grid();
+	    MapList.target[1] = obj.getY_grid();
+
         XMPPSendText("semiauto coordinate start");
         XMPPSendText("semiauto coordinate" +" "+ demo_x +" "+ demo_y);
         XMPPSendText("semiauto coordinate end");
